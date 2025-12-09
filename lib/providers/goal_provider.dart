2365b9f65dfa4c5dc260f7b5e2197f7b9096074d
iamcarolls <marshacaroline128@gmail.com>
2025-12-10 02:23:40 +0800

    chore: add goal provider

---
+++ lib/providers/goal_provider.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../models/goal_model.dart';
+
+class GoalProvider with ChangeNotifier {
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Stream<List<GoalModel>> getGoals() {
+    final uid = _auth.currentUser?.uid;
+    if (uid == null) return const Stream.empty();
+
+    return _db
+        .collection('users')
+        .doc(uid)
+        .collection('goals')
+        .orderBy('createdAt', descending: true)
+        .snapshots()
+        .map((snapshot) => snapshot.docs.map((doc) => GoalModel.fromFirestore(doc)).toList());
+  }
+
+  Future<void> addGoal(GoalModel goal) async {
+    final uid = _auth.currentUser?.uid;
+    if (uid != null) {
+      await _db.collection('users').doc(uid).collection('goals').add(goal.toMap());
+    }
+  }
+
+  Future<void> updateGoal(GoalModel goal) async {
+    final uid = _auth.currentUser?.uid;
+    if (uid != null) {
+      await _db.collection('users').doc(uid).collection('goals').doc(goal.id).update({
+        'title': goal.title,
+        'target': goal.targetAmount,
+        'current': goal.currentAmount,
+        'colorIdx': goal.colorIdx,
+        'isCompleted': goal.isCompleted,
+      });
+    }
+  }
+  
+  Future<void> deleteGoal(String id) async {
+    final uid = _auth.currentUser?.uid;
+    if (uid != null) {
+      await _db.collection('users').doc(uid).collection('goals').doc(id).delete();
+    }
+  }
+}
