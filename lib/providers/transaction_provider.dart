be19501ad28b9516b563097e6e854488b06a0723
iamcarolls <marshacaroline128@gmail.com>
2025-12-10 02:44:34 +0800

    feat: add transaction provider

--- lib/providers/transaction_provider.dart
+++ lib/providers/transaction_provider.dart
@@ -4,20 +4,25 @@ import '../services/firestore_service.dart';
 
 class TransactionProvider with ChangeNotifier {
   final FirestoreService _service = FirestoreService();
-  
+
+  // Getter Stream
   Stream<List<TransactionModel>> get transactionStream => _service.getTransactions();
 
+  // Add Data
   Future<void> addTransaction(TransactionModel tx) async {
     try {
       await _service.addTransaction(tx);
-      notifyListeners(); 
+      notifyListeners();
     } catch (e) {
       rethrow;
     }
   }
 
+  // --- TAMBAHKAN FUNGSI INI (DELETE) ---
   Future<void> deleteTransaction(String id) async {
     try {
+      // Kita panggil service delete (yang akan kita update sebentar lagi)
+      // Atau bisa langsung panggil Firestore di sini jika code sebelumnya digabung
       await _service.deleteTransaction(id);
       notifyListeners();
     } catch (e) {
